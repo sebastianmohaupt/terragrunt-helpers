76aeaee875e17056cd1f8ae45f40bda783be967f
Sebastian Mohaupt <sebastian.mohaupt@hannover-re.com>
2026-01-19 08:51:07 +0100

    Upgraded to terragrunt version 0.91.1

--- terragrunt-helpers.plugin.zsh
+++ terragrunt-helpers.plugin.zsh
@@ -1,5 +1,5 @@
 
-# Function to run "terragrunt COMMAND --terragrunt-source /path/to/your/terraform/module"
+# Function to run "terragrunt COMMAND -source /path/to/your/terraform/module"
 # without providing the path to the terraform module.
 function _tgts() {
     local sub_module sub_module_dir
@@ -10,15 +10,15 @@ function _tgts() {
     fi
     sub_module_dir="${TERRAFORM_MODULES_PATH}/${sub_module}"
     echo "source: $sub_module_dir"
-    terragrunt "$@" --terragrunt-source  $sub_module_dir
+    terragrunt "$@" --source  $sub_module_dir
 }
 
 # Activate the following three exports if you want to use caching.
 # See https://developer.hashicorp.com/terraform/cli/config/config-file#provider-plugin-cache
 # and https://terragrunt.gruntwork.io/docs/features/provider-cache/ 
 export TF_PLUGIN_CACHE_DIR="$HOME/.terraform.d/plugin-cache"
-export TERRAGRUNT_PROVIDER_CACHE=1
-export TERRAGRUNT_PROVIDER_CACHE_DIR=$TF_PLUGIN_CACHE_DIR
+export TG_PROVIDER_CACHE=1
+export TG_PROVIDER_CACHE_DIR=$TF_PLUGIN_CACHE_DIR
 
 alias tg='terragrunt'
 
@@ -26,9 +26,9 @@ alias tga='tg apply'
 alias tgp='tg plan'
 alias tgd='tg destroy'
 
-# alias tgas='tga --terragrunt-source'
-# alias tgps='tgp --terragrunt-source'
-# alias tgds='tgd --terragrunt-source'
+# alias tgas='tga --source'
+# alias tgps='tgp --source'
+# alias tgds='tgd --source'
 alias tgas='_tgts apply'
 alias tgps='_tgts plan'
 alias tgds='_tgts destroy'
@@ -37,11 +37,11 @@ alias tgsps='_tgts state pull'
 alias tgspus='_tgts state push'
 alias tgius='_tgts init -upgrade'
 
-alias tgaall='tg run-all apply --terragrunt-non-interactive --terragrunt-parallelism 10'
-alias tgdall='tgdall(){ local result; read "result?Are you sure (y/n)? "; [[ $result =~ ^[Yy]$ ]] && tg run-all destroy --terragrunt-non-interactive --terragrunt-parallelism 10 "$@" }; noglob tgdall'
-alias tgpall='tg run-all plan --terragrunt-non-interactive --terragrunt-parallelism 10'
-alias tgasall='tgaall --terragrunt-source $(dirname $TERRAFORM_MODULES_PATH)'
-alias tgpsall='tgpall --terragrunt-source $(dirname $TERRAFORM_MODULES_PATH)'
-alias tgdsall='tgdall --terragrunt-source $(dirname $TERRAFORM_MODULES_PATH)'
+alias tgaall='tg run --all apply --non-interactive --parallelism 10'
+alias tgdall='tgdall(){ local result; read "result?Are you sure (y/n)? "; [[ $result =~ ^[Yy]$ ]] && tg run --all destroy --non-interactive --parallelism 10 "$@" }; noglob tgdall'
+alias tgpall='tg run --all plan --non-interactive --parallelism 10'
+alias tgasall='tgaall --source $(dirname $TERRAFORM_MODULES_PATH)'
+alias tgpsall='tgpall --source $(dirname $TERRAFORM_MODULES_PATH)'
+alias tgdsall='tgdall --source $(dirname $TERRAFORM_MODULES_PATH)'
 
 alias clearTerragruntCache='fd -t d -H -I .terragrunt-cache -X rm -rf'
